b23936c98ac1d72b5996ccf6b15ff6a05380fd19
comme-il-faut <rovenkoaa@icloud.com>
2024-03-24 15:39:45 +0300

    sprint 9 begin

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Антон Ровенко on 24.03.2024.
+//
+
+import UIKit
+
+class ProfileViewController {
+    
+    @IBOutlet weak var userPhoto: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var userText: UILabel!
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton(_ sender: Any) {
+    }
+}

8c9af1a4436b9b33e5b09615711b2c2b21b40a2a
comme-il-faut <rovenkoaa@icloud.com>
2024-03-27 17:27:33 +0300

    Спринт 9/20: 9 → Тема 2/6: Навигация → Урок 4/7 +

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Антон Ровенко on 27.03.2024.
+//
+
+import UIKit
+
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
